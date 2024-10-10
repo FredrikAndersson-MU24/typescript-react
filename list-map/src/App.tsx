6e92a76870762b23a300058bb5690ce4286910bf
Fredrik Andersson <3tomu24.andefr@folkuniversitetet.nu>
2024-10-10 15:51:00 +0200

    first commit

---
+++ list-map/src/App.tsx
@@ -0,0 +1,17 @@
+import List from "./List";
+
+// Lista och Map-funktion:
+// Bygg en komponent som renderar en lista med objekt (t.ex. en lista över frukter) och använd map för att visa dem.
+// Fundera på: Hur kan du använda TypeScript för att säkerställa att listobjekten har rätt struktur?
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <List />
+    </>
+  );
+}
+
+export default App;
