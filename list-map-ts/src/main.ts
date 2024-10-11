3b9e158b76406138038e84fe63f4f5ec82891ed5
Fredrik Andersson <3tomu24.andefr@folkuniversitetet.nu>
2024-10-11 14:47:27 +0200

    added HTML elements and functions to add to list

--- list-map-ts/src/main.ts
+++ list-map-ts/src/main.ts
@@ -1,24 +1,52 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const buttonAdd = document.getElementById("add");
+const buttonClear = document.querySelector(".clear");
+const inputName = document.getElementById("name") as HTMLInputElement;
+const inputColor = document.getElementById("color") as HTMLInputElement;
+const inputWeight = document.getElementById("weight") as HTMLInputElement;
+const list = document.getElementById("list") as HTMLUListElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+interface Fruits {
+  name: string;
+  color: string;
+  weight: number;
+}
+
+let fruits: Fruits[] = [];
+const addListElement = (name: string, color: string, weight: number) => {
+  const li = document.createElement("li");
+  const span1 = document.createElement("p");
+  const span2 = document.createElement("p");
+  const span3 = document.createElement("p");
+  li.setAttribute("class", "listItem");
+  li.append(span1, " ", span2, " ", span3);
+  span1.innerText = name;
+  span2.innerText = color;
+  span3.innerText = weight.toString();
+  list.prepend(li);
+};
+
+for (let i = 0; i < fruits.length; i++) {
+  addListElement(fruits[i].name, fruits[i].color, fruits[i].weight);
+}
+
+buttonAdd?.addEventListener("click", () => {
+  const addName = inputName.value;
+  const addColor = inputColor.value;
+  const addWeight = Number(inputWeight.value);
+
+  fruits.push({
+    name: addName,
+    color: addColor,
+    weight: addWeight,
+  });
+  addListElement(addName, addColor, addWeight);
+  console.log(fruits);
+});
+
+buttonClear?.addEventListener("click", () => {
+  fruits = [];
+  list.innerHTML = "";
+  console.log(fruits);
+});
