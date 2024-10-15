3059b209811a81f45b75ea7b731dd153445c9bcf
Fredrik Andersson <3tomu24.andefr@folkuniversitetet.nu>
2024-10-15 12:13:36 +0200

    simplified code

--- fibonacci/src/App.tsx
+++ fibonacci/src/App.tsx
@@ -4,13 +4,6 @@ import "./App.css";
 
 function App() {
   const [array, setArray] = useState<Array<number>>([0, 1]);
-  const two: number = array[array.length - 2];
-  const one: number = array[array.length - 1];
-  const [newNumber, setNewNumber] = useState<number>(array[array.length - 1]);
-
-  useEffect(() => {
-    setArray((a) => [...a, newNumber]);
-  }, [newNumber]);
 
   useEffect(() => {
     window.localStorage.setItem("number", array.toString());
@@ -18,15 +11,22 @@ function App() {
 
   return (
     <>
-      <button onClick={() => setNewNumber(one + two)}>klicka</button>
-      <p>{array[array.length - 1]}</p>
+      <button
+        onClick={() =>
+          setArray((a) => [
+            ...a,
+            array[array.length - 1] + array[array.length - 2],
+          ])
+        }
+      >
+        Clicks: {array.length - 2}
+      </button>
+      <p>
+        {array[array.length - 3]} + {array[array.length - 2]} ={" "}
+        {array[array.length - 1]}
+      </p>
     </>
   );
 }
 
 export default App;
-
-const calculateFibonacci = (n: number): number => {
-  if (n <= 1) return n;
-  return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
-};
