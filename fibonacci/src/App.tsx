13f81a550584480353c36e6dbb00c0ef883bb56f
Fredrik Andersson <3tomu24.andefr@folkuniversitetet.nu>
2024-10-15 15:23:42 +0200

    added local storage functionality

--- fibonacci/src/App.tsx
+++ fibonacci/src/App.tsx
@@ -1,11 +1,25 @@
 import { useEffect, useState } from "react";
-
 import "./App.css";
 
 function App() {
-  const [array, setArray] = useState<Array<number>>([0, 1]);
+  const [array, setArray] = useState<Array<number>>([]);
+
   useEffect(() => {
-    window.localStorage.setItem("number", array.toString());
+    const JSONfromLS = window.localStorage.getItem("number");
+    if (JSONfromLS) {
+      if (JSONfromLS != null) {
+        const tempArray: Array<number> = JSON.parse(JSONfromLS);
+        if (tempArray.length > 0 && array.length === 0) {
+          setArray(tempArray);
+        }
+      }
+    } else {
+      setArray([0, 1]);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("number", JSON.stringify(array));
   }, [array]);
 
   return (
@@ -21,6 +35,14 @@ function App() {
         {array[array.length - 3]} + {array[array.length - 2]} ={" "}
         {array[array.length - 1]}
       </p>
+      <button
+        onClick={() => {
+          setArray([0, 1]);
+          window.localStorage.clear();
+        }}
+      >
+        CLEAR
+      </button>
     </>
   );
 }
