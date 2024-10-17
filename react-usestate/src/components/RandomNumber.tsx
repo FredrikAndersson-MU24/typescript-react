0c29278e33a38e04a271e7303f04d38e9ab09121
Fredrik Andersson <3tomu24.andefr@folkuniversitetet.nu>
2024-10-17 09:00:37 +0200

    changed title in index.html. simplified onClick to DRY in RandomNumber.tsx

--- react-usestate/src/components/RandomNumber.tsx
+++ react-usestate/src/components/RandomNumber.tsx
@@ -4,14 +4,11 @@ import Button from "./Button";
 const RandomNumber = () => {
   const [randomNumber, setRandomNumber] = useState<number>(0);
   const handleRandomNumber = () => {
-    setRandomNumber(Math.random() * 10);
+    setRandomNumber(Math.floor(Math.random() * 10));
   };
   return (
     <>
-      <Button
-        click={() => setRandomNumber(Math.floor(Math.random() * 10))}
-        name="Generate random number"
-      />
+      <Button click={handleRandomNumber} name="Generate random number" />
       <p>{randomNumber}</p>
     </>
   );
